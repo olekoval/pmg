56aae849abcb6d25f218fdd03aaeb28df1fb1480
oleh.koval <314koval.ok@gmail.com>
2025-04-17 15:02:35 +0300

    Configure static file serving with WhiteNoise for production

--- pmg/settings.py
+++ pmg/settings.py
@@ -108,6 +108,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
